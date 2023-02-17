364a24677947a1b520a96318604ec36f68f9b176
Pedro <pedroullmann@hotmail.com>
2023-02-17 17:31:04 -0300

    Add corner radius static values

---
+++ Sources/DesignSystem/DS+CornerRadius.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public extension DesignSystem {
+    struct CornerRadius {
+        /// Radius for extra small rounded corners, 4 points.
+        static let xSmall: CGFloat = 4
+
+        /// Radius for small rounded corners, 8 points.
+        static let small: CGFloat = 8
+
+        /// Radius for medium rounded corners, 16 points.
+        static let medium: CGFloat = 16
+
+        /// Radius for large rounded corners, 24 points.
+        static let large: CGFloat = 24
+    }
+}
