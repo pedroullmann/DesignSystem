20d4202323c4af577be5420e519fc0ae0f447085
Pedro <pedroullmann@hotmail.com>
2023-02-17 17:14:27 -0300

    Init package

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "DesignSystem",
+    products: [
+        .library(
+            name: "DesignSystem",
+            targets: [
+                "DesignSystem"
+            ]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "DesignSystem",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "DesignSystemTests",
+            dependencies: [
+                "DesignSystem"
+            ]
+        ),
+    ]
+)
