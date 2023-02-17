88afe97e8bd08de49b656d1201167ffc52ad4de9
Pedro <pedroullmann@hotmail.com>
2023-02-17 17:18:24 -0300

    Add spacings

--- Sources/DesignSystem/DS+Spacings.swift
+++ Sources/DesignSystem/DS+Spacings.swift
@@ -1,18 +1,26 @@
 import Foundation
 
-public struct Spacings {
-    /// Extra small spacing, 4 points.
-    static let xSmall: CGFloat = 4
-    /// Small spacing, 8 points.
-    static let small: CGFloat = 8
-    /// Medium spacing, 16 points.
-    static let medium: CGFloat = 16
-    /// Large spacing, 24 points.
-    static let large: CGFloat = 24
-    /// Extra large spacing, 32 points.
-    static let xLarge: CGFloat = 32
-    /// Double extra large spacing, 64 points.
-    static let xxLarge: CGFloat = 64
-    /// Triple extra large spacing, 128 points.
-    static let xxxLarge: CGFloat = 128
+public extension DesignSystem {
+    struct Spacings {
+        /// Extra small spacing, 4 points.
+        static let xSmall: CGFloat = 4
+
+        /// Small spacing, 8 points.
+        static let small: CGFloat = 8
+
+        /// Medium spacing, 16 points.
+        static let medium: CGFloat = 16
+
+        /// Large spacing, 24 points.
+        static let large: CGFloat = 24
+
+        /// Extra large spacing, 32 points.
+        static let xLarge: CGFloat = 32
+
+        /// Double extra large spacing, 64 points.
+        static let xxLarge: CGFloat = 64
+
+        /// Triple extra large spacing, 128 points.
+        static let xxxLarge: CGFloat = 128
+    }
 }
